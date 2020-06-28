f7e40fa38591c493a4bef1ae8f3652c8c62ca658
Vishalcross <vishalcross36@gmail.com>
2020-06-28 21:42:06 +0530

    Assignment 1

--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
-import {
-	Card,
-	CardImg,
-	CardImgOverlay,
-	CardText,
-	CardBody,
-	CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import DishDetail from "./DishDetailComponent";
 class Menu extends Component {
 	constructor(props) {
 		super(props);
@@ -26,17 +20,9 @@ class Menu extends Component {
 
 	renderDish(dish) {
 		if (dish != null) {
-			return (
-				<Card>
-					<CardImg width="100%" src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
-			);
-		}
-		return <div></div>;
+			console.log(dish);
+			return <DishDetail dish={dish} />;
+		} else return <div></div>;
 	}
 
 	render() {
@@ -60,9 +46,7 @@ class Menu extends Component {
 		return (
 			<div className="container">
 				<div className="row">{menu}</div>
-				<div className="row">
-					{this.renderDish(this.state.selectedDish)}
-				</div>
+				{this.renderDish(this.state.selectedDish)}
 			</div>
 		);
 	}
