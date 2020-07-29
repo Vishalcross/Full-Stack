0af3a114500d200831b6d9a0c7a8aba23e5bf32c
Vishalcross <vishalcross36@gmail.com>
2020-07-29 13:13:37 +0530

    Callbacks and Errors

--- Node/Node_examples/index.js
+++ Node/Node_examples/index.js
@@ -1,19 +1,21 @@
 var rect = require("./rectangle");
+var rect = require("./rectangle");
 
 function solveRectangle(x, y) {
 	console.log(`Solving for rectangle with length = ${x} and breadth = ${y}`);
-	if (x <= 0 || y <= 0) {
-		console.log(
-			"Rectangle dimensions should be a non-zero positive number"
-		);
-	} else {
-		console.log(
-			`Area is ${rect.area(x, y)} and Perimeter is ${rect.perimeter(
-				x,
-				y
-			)}`
-		);
-	}
+	rect(x, y, (err, rectangle) => {
+		if (err) {
+			console.log("Error: ", err.message);
+		} else {
+			console.log(
+				"Area is " +
+					rectangle.area() +
+					" and perimieter is " +
+					rectangle.perimeter()
+			);
+		}
+	});
+	console.log("This statement is after call to Rect");
 }
 
 solveRectangle(1, 2);
