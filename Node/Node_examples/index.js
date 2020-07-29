2612738025ba29c0f3a6337a0888216a44bb5ab3
Vishalcross <vishalcross36@gmail.com>
2020-07-29 12:18:14 +0530

    First Node example

---
+++ Node/Node_examples/index.js
@@ -0,0 +1,25 @@
+var rect = {
+	perimeter: (x, y) => 2 * (x + y),
+	area: (x, y) => x * y,
+};
+
+function solveRectangle(x, y) {
+	console.log(`Solving for rectangle with length = ${x} and breadth = ${y}`);
+	if (x <= 0 || y <= 0) {
+		console.log(
+			"Rectangle dimensions should be a non-zero positive number"
+		);
+	} else {
+		console.log(
+			`Area is ${rect.area(x, y)} and Perimeter is ${rect.perimeter(
+				x,
+				y
+			)}`
+		);
+	}
+}
+
+solveRectangle(1, 2);
+solveRectangle(3, 5);
+solveRectangle(0, 5);
+solveRectangle(3, -2);
