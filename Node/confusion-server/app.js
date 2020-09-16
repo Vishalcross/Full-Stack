2ff7ca3857760c26638f5ba52ccc60320c1935f7
Vishalcross <vishalcross36@gmail.com>
2020-09-16 16:20:46 +0530

    Final Commit

--- Node/confusion-server/app.js
+++ Node/confusion-server/app.js
@@ -1,137 +1,124 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
-const mongoose = require("mongoose");
-// const { Client } = require("pg");
-// const Sequelize = require("sequelize");
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const dishRouter = require("./routes/dishRouter");
-const promotionsRouter = require("./routes/promotionRouter");
-const leaderRouter = require("./routes/leaderRouter");
-const Dishes = require("./models/dishes");
-const app = express();
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
-app.all("*", (req, res, next) => {
-    if (req.secure) {
-        return next();
-    } else {
-        res.redirect(
-            307,
-            "https://" + req.hostname + ":" + app.get("secureport") + req.url
-        );
-    }
+var index = require("./routes/index");
+var users = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
+const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
+var config = require("./config");
+
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
+
+const Dishes = require("./models/dishes");
+
+var passport = require("passport");
+var authenticate = require("./authenticate");
+
+// Connection URL
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+	useMongoClient: true
+	/* other options */
 });
 
-const url = "mongodb://127.0.0.1:27017/confusion";
-const passport = require("passport");
-const authenticate = require("./authenticate");
-const config = require("./config");
-const connect = mongoose.connect(config.mongoUrl);
-// const sequelize = new Sequelize(
-//     "postgres://postgres:admin@localhost:5432/HRMS"
-// );
-// sequelize
-//     .authenticate()
-//     .then(() => {
-//         console.log("Connection has been established successfully.");
-//     })
-//     .catch((err) => {
-//         console.error("Unable to connect to the database:", err);
-//     });
 connect.then(
-    (db) => {
-        console.log("Connected to the server");
-    },
-    (err) => {
-        console.log(err);
-    }
+	(db) => {
+		console.log("Connected correctly to server");
+	},
+	(err) => {
+		console.log(err);
+	}
 );
-// const User = sequelize.define(
-//     "user",
-//     {
-//         firstName: {
-//             type: Sequelize.STRING,
-//             allowNull: false,
-//         },
-//         lastName: {
-//             type: Sequelize.STRING,
-//         },
-//     },
-//     {}
-// );
-// User.sync({ force: false });
+
+var app = express();
+
+// Secure traffic only
+app.all("*", (req, res, next) => {
+	if (req.secure) {
+		return next();
+	} else {
+		res.redirect(
+			307,
+			"https://" + req.hostname + ":" + app.get("secPort") + req.url
+		);
+	}
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: false
-    })
-);
-// app.use(cookieParser("12345-67890-12345-67890"));
-// app.use(
-//     session({
-//         name: "session-id",
-//         secret: "12345-67890-12345-67890",
-//         saveUninitialized: false,
-//         resave: false,
-//         store: new FileStore()
-//     })
-// );
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
+
 app.use(passport.initialize());
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use(passport.session());
+
+app.use("/", index);
+app.use("/users", users);
+
+// function auth (req, res, next) {
+//   console.log(req.user);
+
+//   if (!req.user) {
+//     var err = new Error('You are not authenticated!');
+//     res.setHeader('WWW-Authenticate', 'Basic');
+//     err.status = 401;
+//     next(err);
+//   }
+//   else {
+//         next();
+//   }
+// }
+
+// app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/leaders", leaderRouter);
-app.use("/promotions", promotionsRouter);
 app.use("/dishes", dishRouter);
-// app.post("/user", async (req, res) => {
-//     try {
-//         const newUser = new User(req.body);
-//         await newUser.save();
-//         res.json({ user: newUser });
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
-// app.get("/user/:userId", async (req, res) => {
-//     const userId = req.params.userId;
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
 
-//     try {
-//         const user = await User.findAll({
-//             where: {
-//                 id: userId,
-//             },
-//         });
-
-//         res.json({ user });
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
